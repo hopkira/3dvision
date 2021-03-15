21ac1f0238f104f63804de663ca6cbc715e79a18
Richard Hopkins <hopkira@googlemail.com>
2021-03-15 21:05:24 +0000

    Update 3dvision.py

--- 3dvision.py
+++ 3dvision.py
@@ -134,11 +134,11 @@ while True: # main loop until 'q' is pressed
                         fps = str(int(fps))
                         pt_t5 = x_1 + 5, y_1 + 140
                         cv2.putText(image_frame, 'fps: ' + fps, pt_t5, cv2.FONT_HERSHEY_SIMPLEX, 0.5, color)
-                        if move_counter == 20:
+                        if move_counter == 10:
                             move_counter = 0
-                            if angle > 0.04:
+                            if (angle > 0.04) :
                                 logo.right(abs(angle))
-                            if angle < 0.04:
+                            if (angle < -0.04) :
                                 logo.left(abs(angle))
                         move_counter += 1
         
