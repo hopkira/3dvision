1b509c4525a8023d0a4fd8caf5956e70a607c162
Richard Hopkins <hopkira@googlemail.com>
2021-03-21 18:25:25 +0000

    Update 3dvision.py

--- 3dvision.py
+++ 3dvision.py
@@ -106,6 +106,12 @@ while True: # main loop until 'q' is pressed
             image_frame = (65535 // image_frame).astype(np.uint8)
             # colorize depth map
             image_frame = cv2.applyColorMap(image_frame, cv2.COLORMAP_HOT)
+
+
+
+
+
+
             if detections is not None:
                 print("There are", str(len(detections)),"objects found by cameras")
                 valid_boxes = []
@@ -125,6 +131,8 @@ while True: # main loop until 'q' is pressed
                 y_min_sum = 0
                 y_max_sum = 0
                 z_sum = 0
+                x_sum = 0
+                y_sum = 0
                 confidence_sum = 0
                 for box in valid_boxes:
                     print("BBox for", str(labels[detections[box].label]),str(box), "at", str(detections[box].depth_z), "m")
@@ -133,14 +141,16 @@ while True: # main loop until 'q' is pressed
                     y_min_sum = y_min_sum + detections[box].y_min
                     y_max_sum = y_max_sum + detections[box].y_max
                     z_sum = z_sum + detections[box].depth_z
+                    x_sum = x_sum + detections[box].depth_x
+                    y_sum = y_sum + detections[box].depth_y
                     confidence_sum = confidence_sum + detections[box].confidence
                 x_min_avg = x_min_sum / num_boxes
                 x_max_avg = x_max_sum / num_boxes
                 y_min_avg = y_min_sum / num_boxes
                 y_max_avg = y_max_sum / num_boxes
                 z_avg = z_sum / num_boxes
-                x_centre = (x_min_avg + x_max_avg) / 2.0
-                y_centre = (x_min_avg + x_max_avg) / 2.0
+                x_avg = x_sum / num_boxes
+                y_avg = y_sum / num_boxes
                 confidence_avg = confidence_sum / num_boxes
                 # convert the resulting box into a bounding box
                 pt1 = nn_to_depth_coord(x_min_avg, y_min_avg, nn2depth)
@@ -152,10 +162,10 @@ while True: # main loop until 'q' is pressed
                 cv2.putText(image_frame, str(score) + ' ' + label,(pt1[0] + 2, pt1[1] + 15),cv2.FONT_HERSHEY_DUPLEX, 0.5, color, 2)  
                 x_1, y_1 = pt1
                 pt_t1 = x_1 + 5, y_1 + 60
-                angle = ( math.pi / 2 ) - math.atan2(z_avg, x_centre)
-                cv2.putText(image_frame, 'x:' '{:7.2f}'.format(x_centre) + ' m', pt_t1, cv2.FONT_HERSHEY_DUPLEX, 0.5, color)
+                angle = ( math.pi / 2 ) - math.atan2(z_avg, x_avg)
+                cv2.putText(image_frame, 'x:' '{:7.2f}'.format(x_avg) + ' m', pt_t1, cv2.FONT_HERSHEY_DUPLEX, 0.5, color)
                 pt_t2 = x_1 + 5, y_1 + 80
-                cv2.putText(image_frame, 'y:' '{:7.2f}'.format(y_centre) + ' m', pt_t2, cv2.FONT_HERSHEY_DUPLEX, 0.5, color)
+                cv2.putText(image_frame, 'y:' '{:7.2f}'.format(y_avg) + ' m', pt_t2, cv2.FONT_HERSHEY_DUPLEX, 0.5, color)
                 pt_t3 = x_1 + 5, y_1 + 100
                 cv2.putText(image_frame, 'z:' '{:7.2f}'.format(z_avg) + ' m', pt_t3, cv2.FONT_HERSHEY_DUPLEX, 0.5, color)
                 pt_t4 = x_1 + 5, y_1 + 120
