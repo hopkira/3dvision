33ce38dd4adb58e7340d35236d614e68eb0244c3
Richard Hopkins <hopkira@googlemail.com>
2021-03-21 17:56:49 +0000

    Update 3dvision.py

--- 3dvision.py
+++ 3dvision.py
@@ -150,7 +150,7 @@ while True: # main loop until 'q' is pressed
                 cv2.putText(image_frame, str(score) + ' ' + label,(pt1[0] + 2, pt1[1] + 15),cv2.FONT_HERSHEY_DUPLEX, 0.5, color, 2)  
                 x_1, y_1 = pt1
                 pt_t1 = x_1 + 5, y_1 + 60
-                angle = ( math.pi / 2 ) - math.atan2(detection.depth_z, detection.depth_x)
+                angle = ( math.pi / 2 ) - math.atan2(z_avg, (x_min_avg + x_max_avg) / 2.0)
                 cv2.putText(image_frame, 'x:' '{:7.2f}'.format(detection.depth_x) + ' m', pt_t1, cv2.FONT_HERSHEY_DUPLEX, 0.5, color)
                 pt_t2 = x_1 + 5, y_1 + 80
                 cv2.putText(image_frame, 'y:' '{:7.2f}'.format(detection.depth_y) + ' m', pt_t2, cv2.FONT_HERSHEY_DUPLEX, 0.5, color)
