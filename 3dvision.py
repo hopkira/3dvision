4cd5e0515e588bb7f07f0bc170db3f0967d37a99
Richard Hopkins <hopkira@googlemail.com>
2021-10-02 12:42:34 +0100

    Update 3dvision.py

--- 3dvision.py
+++ 3dvision.py
@@ -331,9 +331,10 @@ class Moving_Forward(State):
         # if check between the values of x and y is less than
         # SAFETY_MARGIN + MIN DIST, then stop
         check = k9.scan()
-        min_dist = np.amin(check[17:25])
-        if min_dist < SWEET_SPOT:
-            k9.on_event('move_finished')
+        if check:
+            min_dist = np.amin(check[17:25])
+            if min_dist < SWEET_SPOT:
+                k9.on_event('move_finished')
 
     def on_event(self, event):
         if event == 'chefoloff':
@@ -416,50 +417,51 @@ class K9(object):
         Retrieve a 40 element array derived from the 3D camera
         '''
         nnet_packets, data_packets = body_cam.get_available_nnet_and_data_packets()
-        my_packet = [packet for packet in data_packets if packet.stream_name == 'depth']
-        frame = my_packet[-1].getData()
-        # create a specific frame for display
-        image_frame = np.copy(frame)
-        # Process depth map to communicate to robot
-        frame = skim.block_reduce(frame,(decimate,decimate),np.min)
-        height, width = frame.shape
-        # Convert depth map to point cloud with valid depths
-        column, row = np.meshgrid(np.arange(width), np.arange(height), sparse=True)
-        valid = (frame > 200) & (frame < max_dist)
-        z = np.where(valid, frame, 0)
-        x = np.where(valid, (z * (column - cx) /cx / fx) + 120 , max_dist)
-        y = np.where(valid, 325 - (z * (row - cy) / cy / fy) , max_dist)
-        # Flatten point cloud axes
-        z2 = z.flatten()
-        x2 = x.flatten()
-        y2 = y.flatten()
-        # Stack the x, y and z co-ordinates into a single 2D array
-        cloud = np.column_stack((x2,y2,z2))
-        # Filter the array by x and y co-ordinates
-        in_scope = (cloud[:,1]<800) & (cloud[:,1] > 0) & (cloud[:,0]<2000) & (cloud[:,0] > -2000)
-        in_scope = np.repeat(in_scope, 3)
-        in_scope = in_scope.reshape(-1, 3)
-        scope = np.where(in_scope, cloud, np.nan)
-        # Remove invalid rows from array
-        scope = scope[~np.isnan(scope).any(axis=1)]
-        # Index each point into 10cm x and y bins (40 x 8)
-        x_index = pd.cut(scope[:,0], x_bins)
-        y_index = pd.cut(scope[:,1], y_bins)
-        # Place the depth values into the corresponding bin
-        binned_depths = pd.Series(scope[:,2])
-        # Average the depth measures in each bin
-        totals = binned_depths.groupby([y_index, x_index]).mean()
-        # Reshape the bins into a 8 x 40 matrix
-        totals = totals.values.reshape(8,40)
-        # Determine the nearest segment for each of the 40
-        # horizontal segments
-        closest = np.amin(totals, axis = 0 )
-        # Round the to the nearest 10cm
-        closest = np.around(closest,-2)
-        # Turn into a 1D array
-        closest = closest.reshape(1,-1)
-        # print(closest)
-        return closest
+        for packet in data_packets:
+            if packet.stream_name == 'depth':
+                frame = packet.getData()
+                # create a specific frame for display
+                image_frame = np.copy(frame)
+                # Process depth map to communicate to robot
+                frame = skim.block_reduce(frame,(decimate,decimate),np.min)
+                height, width = frame.shape
+                # Convert depth map to point cloud with valid depths
+                column, row = np.meshgrid(np.arange(width), np.arange(height), sparse=True)
+                valid = (frame > 200) & (frame < max_dist)
+                z = np.where(valid, frame, 0)
+                x = np.where(valid, (z * (column - cx) /cx / fx) + 120 , max_dist)
+                y = np.where(valid, 325 - (z * (row - cy) / cy / fy) , max_dist)
+                # Flatten point cloud axes
+                z2 = z.flatten()
+                x2 = x.flatten()
+                y2 = y.flatten()
+                # Stack the x, y and z co-ordinates into a single 2D array
+                cloud = np.column_stack((x2,y2,z2))
+                # Filter the array by x and y co-ordinates
+                in_scope = (cloud[:,1]<800) & (cloud[:,1] > 0) & (cloud[:,0]<2000) & (cloud[:,0] > -2000)
+                in_scope = np.repeat(in_scope, 3)
+                in_scope = in_scope.reshape(-1, 3)
+                scope = np.where(in_scope, cloud, np.nan)
+                # Remove invalid rows from array
+                scope = scope[~np.isnan(scope).any(axis=1)]
+                # Index each point into 10cm x and y bins (40 x 8)
+                x_index = pd.cut(scope[:,0], x_bins)
+                y_index = pd.cut(scope[:,1], y_bins)
+                # Place the depth values into the corresponding bin
+                binned_depths = pd.Series(scope[:,2])
+                # Average the depth measures in each bin
+                totals = binned_depths.groupby([y_index, x_index]).mean()
+                # Reshape the bins into a 8 x 40 matrix
+                totals = totals.values.reshape(8,40)
+                # Determine the nearest segment for each of the 40
+                # horizontal segments
+                closest = np.amin(totals, axis = 0 )
+                # Round the to the nearest 10cm
+                closest = np.around(closest,-2)
+                # Turn into a 1D array
+                closest = closest.reshape(1,-1)
+                # print(closest)
+                return closest
 
     def callback(self, client, userdata, message):
         """
